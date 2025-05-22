446b7617366f22f524acdf85a5184d9d754c1d0d
Shubhradeep Das <shubhramail.das@gmail.com>
2025-05-22 23:43:52 +0530

    Added TODO comment

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,7 +1,7 @@
 return {
   {"nvim-telescope/telescope-ui-select.nvim"},
   {'nvim-telescope/telescope.nvim',
-    dependencies = { "nvim-lua/plenary.nvim" },
+    dependencies = { "nvim-lua/plenary.nvim","folke/todo-comments.nvim" },
     config = function()
       require('telescope').setup({
       extensions = {
@@ -14,6 +14,7 @@ return {
       vim.keymap.set('n','<C-p>',builtin.find_files,{})
       vim.keymap.set('n','<leader>fg',builtin.live_grep,{})
       vim.keymap.set('n', '<leader>f', ':Telescope find_files<CR>', { desc = 'Find files' })
+      vim.keymap.set('n','<leader>ft',':TodoTelescope<CR>',{ desc = 'Find todo comments' })
       require("telescope").load_extension("ui-select")
     end,
   },
